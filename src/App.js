3afcf0e0df5188441cf033acb4ac113212877a2b
sudan bhusal <sudanbhusal20215@gmail.com>
2021-03-02 14:35:27 -0800

    bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Container from './Components/Container';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+    <Container/>
     </div>
   );
 }
---
+++ src/Components/Container.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Container() {
+    return (
+        <div>
+      <div className="jumbotron jumbotron-fluid text-center">
+      <div className="container">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">Click on a header to sort or search below</p>
+      </div>
+    </div>
+        </div>
+    )
+}
+
+export default Container
